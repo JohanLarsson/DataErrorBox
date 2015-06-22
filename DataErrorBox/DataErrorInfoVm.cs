b0e40680e253e35d81bfcd5ddb117a1341006c4c
Johan Larsson <johan_gavle1@hotmail.com>
2015-06-23 01:10:17 +0200

    Sample

---
+++ DataErrorBox/DataErrorInfoVm.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using DataErrorBox.Annotations;
+
+namespace DataErrorBox
+{
+    public class DataErrorInfoVm : INotifyPropertyChanged, IDataErrorInfo
+    {
+        public readonly Errors _errors = new Errors();
+        private string _stringValue;
+        private int _intValue;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string StringValue
+        {
+            get { return _stringValue; }
+            set
+            {
+                if (value == _stringValue) return;
+                _stringValue = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int IntValue
+        {
+            get { return _intValue; }
+            set
+            {
+                if (value == _intValue) return;
+                if (value > 2)
+                {
+                    _errors.Update(">2");
+                }
+                else
+                {
+                    _intValue = value;
+                    _errors.Clear();
+                }
+                OnPropertyChanged();
+            }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                return _errors.Get(columnName);
+            }
+        }
+
+        public string Error
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ DataErrorBox/Errors.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace DataErrorBox
+{
+    public class Errors
+    {
+        private readonly Dictionary<string, string> _errors = new Dictionary<string, string>();
+
+        public void Update(string error, [CallerMemberName] string caller = null)
+        {
+            _errors[caller] = error;
+        }
+
+        public void Clear([CallerMemberName] string caller = null)
+        {
+            _errors[caller] = string.Empty;
+        }
+
+        public string Get(string member)
+        {
+            string error;
+            if (_errors.TryGetValue(member, out error))
+            {
+                return error;
+            }
+            return string.Empty;
+        }
+    }
+}
